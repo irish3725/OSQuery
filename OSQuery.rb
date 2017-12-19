7c6aa3fef9e8fbe6b38a5d1bf745b0dba463261a
alex <irish3725@gmail.com>
2017-12-19 10:46:25 -0700

    grabbed list of monsters that drop object with the rate at which they drop them

--- OSQuery.rb
+++ OSQuery.rb
@@ -1,3 +1,28 @@
 #!/usr/bin/env ruby
 
-puts "Hello World"
+require 'httparty'
+require 'nokogiri'
+require 'json'
+require 'pry'
+
+# get input arguments
+input = ARGV
+# get specific page
+page = input[0]
+# get url for specific page
+url = "http://oldschoolrunescape.wikia.com/wiki/#{page}"
+#url = "http://en.wikipedia.org/wiki/#{page}"
+
+puts(url)
+
+# request page
+page = HTTParty.get(url)
+
+# turn page into Nokogiri object
+parsed_page = Nokogiri::HTML(page)
+
+dropped_by = parsed_page.css(".sortable").css("td").css("a").children.map { |by| by.text }.compact
+rate = parsed_page.css(".sortable").css("td").css("small").children.map { |r| r.text }.compact
+
+puts("Dropped by:#{dropped_by} at rate #{rate}")
+
