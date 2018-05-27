1d888459379bb5819a1ba7eb9fd3314503ed6535
alex <irish3725@gmail.com>
2018-05-27 14:05:22 -0600

    getPages now creates array of HTTParty objects of all useful treasure trails guide pages

--- utils/TreasureTrails.rb
+++ utils/TreasureTrails.rb
@@ -19,6 +19,20 @@ class TreasureTrails
 
     # get all treasure trails guide pages
     def getPages
+        urls = Array.new(6, "http://oldschoolrunescape.wikia.com/wiki/Treasure_Trails/Guide/")
+        urls[0] = urls[0] + "Anagrams"
+        urls[1] = urls[1] + "Challenge_scrolls"
+        urls[2] = urls[2] + "Ciphers"
+        urls[3] = urls[3] + "Coordinates"
+        urls[4] = urls[4] + "Cryptics"
+        urls[5] = urls[5] + "Emote_clues"
+        urls.each {|url| puts(url)}
+        @pages = Array.new(6)
+        urls.each_with_index {|url, index| @pages[index] = HTTParty.get(urls[index])}
+#        @pages.each_with_index { |page, index| page = 
+        
+        @pages.each {|page| if page.include? "This page does not exist. Mayhaps it should?"; puts("invalid url: #{urls[index]}") end}
+#        @pages.each {|page| puts(page)} 
         puts("getPages not yet implemented")
     end # -- end getPages
 
