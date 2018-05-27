f49118cd37e5a1d37d088c81014cb8ddfbd4b17e
alex <irish3725@gmail.com>
2018-05-27 14:17:29 -0600

    changed searchPages to findPage, now finds the page and calls printSolution giving the page, url, and clue as arguments

--- utils/TreasureTrails.rb
+++ utils/TreasureTrails.rb
@@ -13,36 +13,37 @@ class TreasureTrails
     def run
         puts("arguments: #{@arguments}")
         getPages
-        searchPages
-        printSolution
+        findPage
+        #printSolution
     end # -- end run
 
     # get all treasure trails guide pages
     def getPages
-        urls = Array.new(6, "http://oldschoolrunescape.wikia.com/wiki/Treasure_Trails/Guide/")
-        urls[0] = urls[0] + "Anagrams"
-        urls[1] = urls[1] + "Challenge_scrolls"
-        urls[2] = urls[2] + "Ciphers"
-        urls[3] = urls[3] + "Coordinates"
-        urls[4] = urls[4] + "Cryptics"
-        urls[5] = urls[5] + "Emote_clues"
-        urls.each {|url| puts(url)}
+        @urls = Array.new(6, "http://oldschoolrunescape.wikia.com/wiki/Treasure_Trails/Guide/")
+        @urls[0] = @urls[0] + "Anagrams"
+        @urls[1] = @urls[1] + "Challenge_scrolls"
+        @urls[2] = @urls[2] + "Ciphers"
+        @urls[3] = @urls[3] + "Coordinates"
+        @urls[4] = @urls[4] + "Cryptics"
+        @urls[5] = @urls[5] + "Emote_clues"
+        @urls.each {|url| puts(url)}
         @pages = Array.new(6)
-        urls.each_with_index {|url, index| @pages[index] = HTTParty.get(urls[index])}
+        @urls.each_with_index {|url, index| @pages[index] = HTTParty.get(@urls[index])}
 #        @pages.each_with_index { |page, index| page = 
         
-        @pages.each {|page| if page.include? "This page does not exist. Mayhaps it should?"; puts("invalid url: #{urls[index]}") end}
-#        @pages.each {|page| puts(page)} 
-        puts("getPages not yet implemented")
+        @pages.each {|page| if page.include? "This page does not exist. Mayhaps it should?"; puts("invalid url: #{@urls[index]}") end}
     end # -- end getPages
 
     # search all pages for clue
-    def searchPages
-        puts("searchPages not yet implemented")
-    end # -- end searchPages
+    def findPage
+        puts("searching for clue: #{@arguments}")
+        @pages.each_with_index {|page, index| if page.include? @arguments; printSolution(page, @urls[index], @arguments) end}
+        puts("findPage not yet implemented")
+    end # -- end findPage
 
     # prints solution
-    def printSolution
+    def printSolution(page, url, clue)
+        puts("url: #{url}\nclue: #{clue}")
         puts("printSolution not yet implemented")
     end # -- end printSolution
 
