98e7326be1e2018762a8079360c0a26e6b2f8c82
alex <irish3725@gmail.com>
2018-05-27 13:18:40 -0600

    added method headers for TreasureTrails file

--- utils/TreasureTrails.rb
+++ utils/TreasureTrails.rb
@@ -7,11 +7,30 @@ class TreasureTrails
     #initialize with arguments
     def initialize(arguments)
         @arguments = arguments
-    end
+    end # -- end initialize
 
     #run TreasureTrails object
     def run
         getPages
         searchPages
         printSolution
-    end
+    end # -- end run
+
+    # get all treasure trails guide pages
+    def getPages
+        puts("getPages not yet implemented")
+    end # -- end getPages
+
+    # search all pages for clue
+    def searchPages
+        puts("searchPages not yet implemented")
+    end # -- end searchPages
+
+    # prints solution
+    def printSolution
+        puts("printSolution not yet implemented")
+    end # -- end printSolution
+
+end # -- end TreasureTrails class
+
+
